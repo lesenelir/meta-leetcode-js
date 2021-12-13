c1021f7a2b46e9dc73d3f015e958e5f562b2c040
lesenelir <miaomiaobabyzy@gmail.com>
2021-12-13 13:13:09 +0800

    update Prepare

--- src/0000.Prepare/PrepareStandardLibrary.js
+++ src/0000.Prepare/PrepareStandardLibrary.js
@@ -138,11 +138,94 @@ console.log(v1.toString())
 console.log(v2.toString())
 console.log(v3.toString())
 
-//
+// 4. Number 对象的实例方法
+// 4.1 Number.prototype.toString()   将十进制的数转换为其他进制的字符串
+console.log((10).toString(2))
+console.log((10).toString(8))
+
+// 4.2 Number.prototype.toFixed()   将一个数转换为指定位数的小数，并返回这个小数的字符串
+console.log((10.005).toFixed(2))  // 10.01
+
+// 4.3 Number.prototype.toExponential()    将一个数转换为科学计数法形式
+console.log((10).toExponential())
+console.log((10).toExponential(1))
+console.log((10).toExponential(2))
+
+// 4.4 Number.prototype.toPrecision()      将一个数转为指定位数的有效数字
+console.log((12.34).toPrecision(1)) // "1e+1"
+console.log((12.34).toPrecision(2)) // "12"
+console.log((12.34).toPrecision(3)) // "12.3"
+console.log((12.34).toPrecision(4)) // "12.34"
+console.log((12.34).toPrecision(5)) // "12.340"
+
+// 4.5 Number自定义方法
+// Number.prototype对象上可以自定义方法，被Number的实例继承调用   【重要、重要、重要】
+Number.prototype.iterate = function () {
+  var result = []
+  for (let i = 0; i <= this; i++) {
+    result.push(i)
+  }
+  return result
+}
+console.log((8).iterate())  // [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
+// 5. String 对象，用来生成字符串对象
+var str1 = new String('abc')
+console.log(str1)   // [String: 'abc']
+console.log(str1.valueOf())  // abc
+// Note: 字符串对象是一个类似于数组的对象（很像数组，但不是数组）
+// String {0: "a", 1: "b", 2: "c", length: 3}
 
+// 5.1 String.prototype.charAt()   返回指定位置的字符
+console.log(str1.charAt(1))
+console.log(str1.charAt(str1.length - 1))
 
+// 5.2 String.prototype.charCodeAt()  返回字符串指定位置的Unicode编码
+console.log(str1.charCodeAt())
+console.log(str1.charCodeAt(0))
+console.log(str1.charCodeAt(1))
 
+// 5.3 String.prototype.concat()  连接两个字符串，并返回新的字符串
+var s1 = "hello"
+var s2 = "world"
+var s3 = s1.concat(" ", s2)  // 可以接受多个参数
+console.log(s1)
+console.log(s3)
 
+// 5.4 String.prototype.slice(start, end)   取出某个字符串的子字符串并返回
+console.log(s1.slice(2))  // llo
+console.log(s1.slice(2, 4)) // ll
 
+// 5.5 String.prototype.substr(start, length)  取出某个字符串的子字符串
+console.log(s1.substr(1, 4)) // ello
+
+// 5.6 String.prototype.indexOf() & String.prototype.lastIndexOf()
+// 确定一个字符串在另一个字符串中第一次出现的位置，返回结果是匹配开始的位置
+console.log('hello world'.indexOf('o'))      // 4
+console.log('JavaScript'.indexOf('script'))   // -1
+
+// 5.7 String.prototype.trim()    取出字符串首尾的空格制表符等
+console.log('      \r\nabc \t     '.trim())
+
+// 5.8 String.prototype.split()     按照规定分割字符串，返回一个分割出来的子字符串组成的数组 ---- 第二个参数返回数组长度
+var arr = "a-b-c-d-e-4".split("-")
+console.log(arr) // [ 'a', 'b', 'c', 'd', 'e', '4' ]
+var arrTest58 = "a-b-c-d-e-4".split("-", 5)
+console.log(arrTest58) // [ 'a', 'b', 'c', 'd', 'e' ]
+
+// 6. Math 对象
+// Math.random() 返回一个0到1的随机数
+// 6.1 任意范围随机数生成函数
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+console.log(getRandomArbitrary(1.5, 6.5))
+
+// 6.2 任意范围随机整数生成函数
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+console.log(getRandomInt(1, 6))
