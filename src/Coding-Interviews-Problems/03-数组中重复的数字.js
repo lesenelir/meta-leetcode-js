4a74bf75ebfd10d1c6cd4e75382fe4ed2c11da5c
lesenelir <miaomiaobabyzy@gmail.com>
2021-12-20 14:10:54 +0800

    Update 数组中重复的数组

--- src/Coding-Interviews-Problems/03-数组中重复的数字.js
+++ src/Coding-Interviews-Problems/03-数组中重复的数字.js
@@ -14,6 +14,23 @@ var findRepeatNumber = function(nums) {
     }
     map.set(nums[i], i)
   }
+  return -1
 };
 
 
+// 方法二2
+var findRepeatNumber2 = function(nums) {
+  // nums.sort(function (a, b) {
+  //   return a - b
+  // })
+  // 对数组进行升序排序
+  nums.sort((a, b) => a- b)
+  for (let i = 1; i < nums.length; i++) {  // 从第一个元素开始查找，而不是从第0个元素开始查找
+    if (nums[i] === nums[i-1]) {
+      return nums[i]
+    }
+  }
+  return -1
+};
+
+console.log(findRepeatNumber2([1,4,3,5]))
