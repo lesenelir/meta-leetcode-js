e36d9848a6d066077c44c1ee137c636e31e05d1b
lesenelir <miaomiaobabyzy@gmail.com>
2022-02-16 20:27:10 +0800

    update README

---
+++ src/Top-interview-questions-easy/01-array/01-删除排序数组中的重复项.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/**
+ *    删除排序中的重复项 ： 快慢指针
+ *      快慢指针的重点：slow保存下一个将要赋值的下标位置，fast指针用来遍历数组根据条件找元素
+ *
+ */
+
+var removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0
+  let slow = 1
+  for (let fast = slow; fast < nums.length; fast++) {
+    if (nums[fast] !== nums[slow - 1]) {
+      nums[slow] = nums[fast]
+      slow++
+    }
+  }
+
+  return slow
+};
---
+++ src/Top-interview-questions-easy/README.md
@@ -0,0 +1,5 @@
+### Top interview questions easy
+
+
+This page Leetcode problems from:
+[Top interview questions easy](https://leetcode-cn.com/leetbook/detail/top-interview-questions-easy/)
