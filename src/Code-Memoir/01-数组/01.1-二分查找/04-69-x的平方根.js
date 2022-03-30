cf271ea52e537951734bd4665fefb4c087d9e43c
lesenelir <miaomiaobabyzy@gmail.com>
2022-03-30 15:12:43 +0800

    update: binary search - 2022-03-30

--- src/Code-Memoir/01-数组/01.1-二分查找/04-69-x的平方根.js
+++ src/Code-Memoir/01-数组/01.1-二分查找/04-69-x的平方根.js
@@ -6,15 +6,17 @@ var mySqrt = function(x) {
   let [left, right] = [1, x]
   while (left <= right) {
     let mid = Math.floor((left + right) / 2)
-    if (mid * mid <= x) {
-      if (((mid + 1) * (mid + 1)) > x) {
+    if (mid * mid === x) {
+      return mid
+    } else if (mid * mid < x) {
+      if (((mid + 1) * (mid + 1)) > x) { // 此时mid+1相乘大于该数，则返回mid为结果值
         return mid
+      } else { // 往大查找
+        left = mid + 1
       }
-      left = mid + 1
-    } else { // 大于该数字的进行缩减范围
+    } else { // 往小查找
       right = mid - 1
     }
-
   }
   return 0 // 输入0则直接返回
 };
