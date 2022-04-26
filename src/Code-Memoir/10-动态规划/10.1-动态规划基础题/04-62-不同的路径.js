ddd7d5a518ef9024703bfe97fdcd336986b62f84
lesenelir <miaomiaobabyzy@gmail.com>
2022-04-26 11:06:22 +0800

    update

--- src/Code-Memoir/10-动态规划/10.1-动态规划基础题/04-62-不同的路径.js
+++ src/Code-Memoir/10-动态规划/10.1-动态规划基础题/04-62-不同的路径.js
@@ -17,8 +17,8 @@ var uniquePaths = function (m, n) {
     dp[0][i] = 1
   }
   // 状态转移方程
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
       dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
     }
   }
