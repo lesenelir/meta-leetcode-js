eb5e94c801fdc37ab8b5f918b6dcad5372e9ef51
lesenelir <miaomiaobabyzy@gmail.com>
2022-04-28 12:48:52 +0800

    update

--- src/Code-Memoir/04-字符串/04.1-字符串基本操作/06-14-最长公共前缀.js
+++ src/Code-Memoir/04-字符串/04.1-字符串基本操作/06-14-最长公共前缀.js
@@ -15,3 +15,23 @@ var longestCommonPrefix = function(strs) {
   }
   return strs[0]
 };
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix2 = function(strs) {
+  if (strs.length === 0) return ''
+  let res = ''
+  for (let j = 0; j < strs[0].length; j++) { // 遍历数组第一个元素内的字符 - 第j位
+    // strs[0]是第一个元素
+    let charItem = strs[0][j]
+    for (let i = 1; i < strs.length; i++) { // 第i个
+      if (strs[i][j] !== charItem) {
+        return res
+      }
+    }
+    res += strs[0][j]
+  }
+  return res
+};
