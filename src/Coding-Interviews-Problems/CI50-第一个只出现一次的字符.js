76693571ee53045868c6c3e368be3597d9935bf3
lesenelir <miaomiaobabyzy@gmail.com>
2022-05-01 20:05:36 +0800

    update

---
+++ src/Coding-Interviews-Problems/CI50-第一个只出现一次的字符.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function(s) {
+  if (s.length === 0) return ' '
+
+  let map = new Map()
+  for (let item of s) {
+    if (map.has(item)) {
+      map.set(item, map.get(item) + 1)
+    } else {
+      map.set(item, 1)
+    }
+  }
+
+  // 已经存储完map
+  for (let [key, value] of map.entries()) {
+    if (value === 1) {
+      return key
+    }
+  }
+  // 没有找到
+  return ' '
+};
