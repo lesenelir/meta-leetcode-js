17cd2d9d25e5404510e1319eaad62026472cfbf8
lesenelir <miaomiaobabyzy@gmail.com>
2022-06-26 12:22:39 +0800

    udpate

--- src/Code-Memoir/10-动态规划/10.2-01背包问题/02-01背包一维数组.js
+++ src/Code-Memoir/10-动态规划/10.2-01背包问题/02-01背包一维数组.js
@@ -5,6 +5,9 @@ function fn() {
 
   // 处理01背包问题
   function zeroOneBagProblem() {
+    // dp[j] 表示 容量为j的背包，所能装的最大价值是dp[j]
+    // dp[j - weight[i]] 表示容量为j - weight[i]所能装的最大价值
+
     const len = weight.length
     // const dp = new Array(weight.length + 1).fill(0).map(() => new Array(bagSize + 1).fill(0)) // 二维数组表示
     const dp = new Array(bagSize + 1).fill(0)
@@ -22,3 +25,16 @@ function fn() {
 fn()
 
 // dp[j] 表示背包容量为j时，能获得的最大价值
+
+function fn2(weight, value, bagSize) {
+  const dp = new Array(bagSize + 1)
+
+  for (let i = 0; i < weight.length; i++) { // 遍历物品
+    for (let j = bagSize; j >= weight[i]; j--) { // 逆序遍历背包
+      dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i])
+    }
+  }
+
+  return dp[bagSize]
+}
+
--- src/Code-Memoir/10-动态规划/10.2-01背包问题/03-416-分割等和子集.js
+++ src/Code-Memoir/10-动态规划/10.2-01背包问题/03-416-分割等和子集.js
@@ -3,15 +3,18 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
-  let sum = nums.reduce((accumulator, item) => {
-    accumulator += item
-    return accumulator
+  // 背包体积是sum / 2 ; 物品的价值和重量都是数字值
+  // 正好装满背包 则有两个相同的子集元素和
+  // dp[j] 背包容量为j 可以凑成j的最大子集总和是dp[j]
+  let sum = nums.reduce((pre, item) => {
+    pre += item
+    return pre
   }, 0)
 
   let bagSize = Math.floor(sum / 2)
 
   // 初始化，01背包如果没有负值，则都可以初始化为0
-  const dp = new Array(Math.floor(bagSize) + 1).fill(0)
+  const dp = new Array(bagSize + 1).fill(0)
 
   for (let i = 0; i < nums.length; i++) { // 物品数是数组中元素
     for (let j = bagSize; j >= nums[i]; j--) {
