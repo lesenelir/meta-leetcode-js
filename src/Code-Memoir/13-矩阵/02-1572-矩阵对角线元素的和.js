0f22514d37e572203baa32e54e470497f6a554d9
lesenelir <miaomiaobabyzy@gmail.com>
2022-07-03 15:25:33 +0800

    update

--- src/Code-Memoir/13-矩阵/02-1572-矩阵对角线元素的和.js
+++ src/Code-Memoir/13-矩阵/02-1572-矩阵对角线元素的和.js
@@ -3,34 +3,38 @@
  * @return {number}
  */
 var diagonalSum = function(mat) {
-  let sum = 0
+  let sum = 0,
+      row = mat.length,
+      col = mat[0].length
 
-  // 找起始坐标
-  if (mat.length % 2 === 0) { // 偶数
+  if (row % 2  === 0) {
     outputMain(0, 0)
-    outputSub(0, mat[0].length - 1)
-    console.log(sum)
-  } else {  // 奇数
+    outputSub(0, col - 1)
+  } else {
+    const mid = Math.floor(row / 2)
     outputMain(0, 0)
-    outputSub(0, mat[0].length - 1)
-    const mid = Math.floor(mat.length / 2)
+    outputSub(0, col - 1)
     sum -= mat[mid][mid]
   }
+
   return sum
 
+  // 从[0][0]遍历主对角线至[row - 1][col - 1]
   function outputMain(i, j) {
-    while (i < mat.length && j < mat[0].length) {
+    while (i < row && j < col) {
       sum += mat[i][j]
       i++
       j++
     }
   }
 
+  // 从[0][col - 1]遍历副对角线至[row - 1][0]
   function outputSub(i, j) {
-    while (i < mat.length && j >= 0) {
+    while (i < row && j >= 0) {
       sum += mat[i][j]
       i++
       j--
     }
   }
+
 };
