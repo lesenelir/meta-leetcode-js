3234095cd3782b4b947efe09ed0d216d09a32f5a
lesenelir <miaomiaobabyzy@gmail.com>
2022-07-04 11:55:14 +0800

    udpate

---
+++ src/Code-Memoir/16-额外题目/17-116-填充每个节点的下一个右侧节点指针.js
@@ -0,0 +1,42 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+  // 层序遍历
+  if (!root) return root
+
+  let queue = []
+  if (root) {
+    queue.push(root)
+    root.next = null
+  }
+
+  while (queue.length) {
+    let levelCount = queue.length
+
+    for (let i = 0; i < levelCount; i++) {
+      let node = queue.shift()
+      if (i !== levelCount - 1) {
+        node.next = queue[0]
+      } else {
+        node.next = null
+      }
+
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+  }
+
+  return root
+};
