5bfe488370237b26ad9e323e701012d2e0996719
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-26 12:00:28 +0800

    update

--- src/Code-Memoir/07-二叉树/07.4-二叉树的递归/17-236-二叉树的最近公共祖先.js
+++ src/Code-Memoir/07-二叉树/07.4-二叉树的递归/17-236-二叉树的最近公共祖先.js
@@ -12,6 +12,27 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+  // 二叉树的递归问题可以分解为：1. 二叉树的遍历解决  2. 将二叉树分解为子问题（子树）
+  // 找最近公共祖先，需要从底向上遍历，只能通过“后序遍历”
+  return traversal(root, p, q)
+
+  // 函数定义：输入node，p，q 返回这个node节点为二叉树的p，q最近的公共祖先节点（返回一个节点）
+  function traversal(node, p, q) {
+    if (!node) return null
+    if (node === p || node === q) return node
+
+    let left = traversal(node.left, p, q) // node的左子树
+    let right = traversal(node.right, p, q) // node的右子树
+    // 后序位置
+    if (left && right) return node
+    if (left && !right) return left
+    if (!left && right) return right
+  }
+
+};
+
+
+var lowestCommonAncestor2 = function(root, p, q) {
   // 思路：使用后序遍历方式，从底向上遍历
   // 后序遍历是天然的回溯过程
   return traversal(root, p, q)
