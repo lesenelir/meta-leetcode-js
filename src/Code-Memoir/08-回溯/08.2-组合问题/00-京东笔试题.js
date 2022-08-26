7eead40347eed3ea71e663b87394e0780b97ff50
lesenelir <miaomiaobabyzy@gmail.com>
2022-08-26 17:07:29 +0800

    update

--- src/Code-Memoir/08-回溯/08.2-组合问题/00-京东笔试题.js
+++ src/Code-Memoir/08-回溯/08.2-组合问题/00-京东笔试题.js
@@ -3,28 +3,31 @@
 // 输入 [1, 4, 9, 19] [7, 8, 15, 17]
 
 
-function problemSolve(arr1, arr2) {
+// function problemSolve(arr1, arr2) {
+//
+//
+//
+// }
 
-  let res = 0
-  let path = []
-  backTracking()
+console.log(getCombine([1, 4, 9, 19]))
+
+// 首先求一个数组的子集
+function getCombine(arr) {
+  let res = [],
+      path = []
+
+  traversal(0)
   return res
 
-  function backTracking(arr, path, startIndex) {
-    if (startIndex === arr.length) {
-
-
-    }
-
+  function traversal(startIndex) {
+    res.push([...path])
     for (let i = startIndex; i < arr.length; i++) {
-
-
+      path.push(arr[i])
+      traversal(i + 1)
+      path.pop()
     }
-
 
   }
 
-
 }
-
 
