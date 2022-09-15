81085c2c6e9251c1e550928d7680054e7f2e6d89
lesenelir <miaomiaobabyzy@gmail.com>
2022-09-15 12:47:15 +0800

    update

--- src/Code-Memoir/15-单调栈/04-42-接雨水.js
+++ src/Code-Memoir/15-单调栈/04-42-接雨水.js
@@ -3,6 +3,29 @@
  * @return {number}
  */
 var trap = function(height) {
+  // 单调栈，维护一个单调递减的栈
+  // 当前高度小于栈顶高度则入栈下标，当前高度大于栈顶高度，说明可以接雨水
+  let res = 0,
+      stack = [],
+      len = height.length
+
+  for (let i = 0; i < len; i++) {
+    // 当前高度大于栈顶元素高度时
+    while (stack.length !== 0 && height[i] > height[stack[stack.length - 1]]) {
+      let bottom = stack.pop() // 底部
+      if (stack.length === 0) break // 没有左墙 break
+      let right = i
+      let left = stack[stack.length - 1]
+      let curHeight = Math.min(height[left], height[right]) - height[bottom]
+      res += (right - left - 1) * curHeight
+    }
+    stack.push(i)
+  }
+
+  return res
+};
+
+var trap2 = function(height) {
   // 单调栈： 维护一个从栈底到栈顶-从大到小的栈
   let stack = []
   let res = 0
