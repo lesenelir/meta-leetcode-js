d1000372bc7566dcebc893cc8d8a7d7b04730113
lesenelir <miaomiaobabyzy@gmail.com>
2023-02-17 16:37:41 +0800

    update

--- src/Code-Memoir/06-栈与队列/06.1-栈与队列基本操作/05-347-前K个高频元素.js
+++ src/Code-Memoir/06-栈与队列/06.1-栈与队列基本操作/05-347-前K个高频元素.js
@@ -34,3 +34,23 @@ var topKFrequent = function(nums, k) {
 let map = {1: 3, 2: 2, 3: 1}
 console.log(Array.from(map))
 
+var topKFrequent2 = function(nums, k) {
+  let len = nums.length,
+    map = new Map(),
+    res = []
+
+  for (let i = 0; i < len; i++) {
+    if (!map.has(nums[i])) map.set(nums[i], 1)
+    else map.set(nums[i], map.get(nums[i]) + 1)
+  }
+
+  // arr 是 一个二维数组
+  let arr = [...map.entries()]
+  arr.sort((a, b) => b[1] - a[1])
+
+  for (let i = 0; i < k; i++) {
+    res.push(arr[i][0])
+  }
+
+  return res
+};
