b4d2f1582944fdf7784b9d9f419ff386d3240224
lesenelir <miaomiaobabyzy@gmail.com>
2023-02-27 11:44:56 +0800

    update

--- src/Code-Memoir/09-贪心/02.3-贪心股票/02-121-买卖股票的最佳时机.js
+++ src/Code-Memoir/09-贪心/02.3-贪心股票/02-121-买卖股票的最佳时机.js
@@ -5,7 +5,7 @@
 var maxProfit = function(prices) {
   let len = prices.length,
       res = 0,
-      low = 0
+      low = Number.MAX_VALUE
 
   for (let i = 0; i < len; i++) {
     low = Math.min(low, prices[i])
