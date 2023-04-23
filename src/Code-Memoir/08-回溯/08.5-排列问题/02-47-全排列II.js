33d7a3253e8248bc0c1c246b0e73f2d54cdf4e6a
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-23 20:42:51 +0800

    update

--- src/Code-Memoir/08-回溯/08.5-排列问题/02-47-全排列II.js
+++ src/Code-Memoir/08-回溯/08.5-排列问题/02-47-全排列II.js
@@ -22,6 +22,7 @@ var permuteUnique = function(nums) {
     // 单层递归逻辑
     for (let i = 0; i < nums.length; i++) {
       // 同一个树层没有遍历递归过，且当前的元素和前一个元素值相等 （需要对原数组进行排序）
+      // 当前元素值相等，且当前元素相等的前一个元素值是false，保证不是在同一个树枝上
       if (i > 0 && nums[i] === nums[i-1] && used[i-1] === false) continue
       if (used[i] === true) continue
       path.push(nums[i])
