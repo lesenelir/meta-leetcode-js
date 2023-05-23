46540b1ce29569e2d87d858a2221c0d7a02a330e
lesenelir <miaomiaobabyzy@gmail.com>
2023-05-23 20:17:46 +0800

    update

---
+++ src/Code-Memoir/00-前端/2627-函数防抖.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+  let timer = null
+  return function(...args) {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn(...args)
+    }, t)
+  }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
