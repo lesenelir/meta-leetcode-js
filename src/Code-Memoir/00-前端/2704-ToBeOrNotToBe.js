54ddb4272ffc17b34da4773e8956c75cec5c6d0c
lesenelir <miaomiaobabyzy@gmail.com>
2023-05-31 22:45:21 +0800

    update

---
+++ src/Code-Memoir/00-前端/2704-ToBeOrNotToBe.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+  return {
+    toBe(v) {
+      if (v === val) return true
+      else throw 'Not Equal'
+    },
+    notToBe(v) {
+      if (v !== val) return true
+      else throw 'Equal'
+    }
+  }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
