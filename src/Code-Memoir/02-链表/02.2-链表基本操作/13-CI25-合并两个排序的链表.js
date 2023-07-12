91b40f74e793dff72e43cb5d95c80bd6ad14060e
lesenelir <miaomiaobabyzy@gmail.com>
2023-07-12 14:32:44 +0800

    update

--- src/Code-Memoir/02-链表/02.2-链表基本操作/13-CI25-合并两个排序的链表.js
+++ src/Code-Memoir/02-链表/02.2-链表基本操作/13-CI25-合并两个排序的链表.js
@@ -42,3 +42,49 @@ var mergeTwoLists = function(l1, l2) {
 
   return newList.next
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists2 = function(list1, list2) {
+  let newList = new ListNode(0, null),
+    cur = newList,
+    p1 = list1,
+    p2 = list2
+
+  while (p1 && p2) {
+    if (p1.val <= p2.val) {
+      cur.next = p1
+      p1 = p1.next
+    } else {
+      cur.next = p2
+      p2 = p2.next
+    }
+    cur = cur.next
+  }
+
+  while (p1) {
+    cur.next = p1
+    p1 = p1.next
+    cur = cur.next
+  }
+
+  while (p2) {
+    cur.next = p2
+    p2 = p2.next
+    cur = cur.next
+  }
+
+  return newList.next
+};
