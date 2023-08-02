2670f3efad395d13b66fbdfd9e823719c146f763
lesenelir <miaomiaobabyzy@gmail.com>
2023-08-02 17:43:06 +0800

    update

--- src/Code-Memoir/10-动态规划/10.1-动态规划基础题/03-746-使用最小花费爬楼梯.js
+++ src/Code-Memoir/10-动态规划/10.1-动态规划基础题/03-746-使用最小花费爬楼梯.js
@@ -13,3 +13,17 @@ var minCostClimbingStairs = function (cost) {
   return Math.min(dp[cost.length - 1], dp[cost.length - 2])
 };
 
+var minCostClimbingStairs2 = function(cost) {
+  // dp[i] 代表爬到第 i 层，需要花费 dp[i] 费用
+  let len = cost.length,
+    dp = new Array(len)
+
+  dp[0] = cost[0]
+  dp[1] = cost[1]
+
+  for (let i = 2; i < len; i++) {
+    dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i]
+  }
+
+  return Math.min(dp[len - 1], dp[len - 2])
+};
